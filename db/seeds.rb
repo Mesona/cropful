2657d14808d0e117c824b0f8aaead0893856e6e3
mesona <kylemesona@gmail.com>
2019-07-12 15:47:52 -0700

    Add test seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Hearvest.destroy_all
+
+test_harvest_1 = Harvest.create!(harvest_type: 'None', ripe: 'false', lat: 37.298703, lng: -121.987777)
+test_harvest_1 = Harvest.create!(harvest_type: 'None', ripe: 'false', lat: 37.299522, lng: -121.989247)
+test_harvest_1 = Harvest.create!(harvest_type: 'None', ripe: 'false', lat: 37.299906, lng: -121.986071)
+
+harvest1 = Harvest.create!(harvest_type: 'Lemon', ripe: 'false', lat: 37.299462, lng: -121.987637)
